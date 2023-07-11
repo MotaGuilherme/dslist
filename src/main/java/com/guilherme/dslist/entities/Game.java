db8134c25f3ed93e7070f14248324b9552c9caf1
MotaGuilherme <metatronmota@gmail.com>
2023-07-11 19:51:53 -0300

    Game seed

--- src/main/java/com/guilherme/dslist/entities/Game.java
+++ src/main/java/com/guilherme/dslist/entities/Game.java
@@ -14,9 +14,14 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataforms;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
@@ -24,12 +29,14 @@ public class Game {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score,
+                String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -68,11 +75,19 @@ public class Game {
     }
 
     public String getPlataforms() {
-        return plataforms;
+        return platforms;
     }
 
-    public void setPlataforms(String plataforms) {
-        this.plataforms = plataforms;
+    public void setPlataforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
